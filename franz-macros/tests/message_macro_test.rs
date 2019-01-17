91efab7fd88c7ad8402df584a5110c401db662f4
Kosta Welke <theoneandonlykosta@gmail.com>
2019-01-18 00:12:08 +0100

    message! macro weirdness solved, now it's just down to string parsing?

--- franz-macros/tests/message_macro_test.rs
+++ franz-macros/tests/message_macro_test.rs
@@ -8,4 +8,6 @@ message!("Foo");
 fn test_message() {
     let f: Foo = franz_base::FromBytes::read(&mut Cursor::new(Bytes::from(&b""[..]))).unwrap();
     assert_eq!(franz_base::ToBytes::len_to_write(&f), 0);
+    assert_eq!(format!("foo: {:?}", f), "foo: Foo");
+    assert_eq!(f, Foo{});
 }
