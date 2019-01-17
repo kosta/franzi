0a6c1db5d81eba8e0a92eb6fb20bcca19619a988
Kosta Welke <theoneandonlykosta@gmail.com>
2019-01-17 22:59:48 +0100

    derive ALL the messages :)

--- franz-proto/src/header.rs
+++ franz-proto/src/header.rs
@@ -3,7 +3,7 @@ use franz_base::types::KafkaString;
 use franz_base::{FromBytes, FromBytesError, ToBytes};
 use std::io::Cursor;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, FromBytes, ToBytes)]
 pub struct RequestHeader {
     pub api_key: i16,
     pub api_version: i16,
@@ -11,54 +11,7 @@ pub struct RequestHeader {
     pub client_id: Option<KafkaString>,
 }
 
-// TODO: Derive these!
-impl FromBytes for RequestHeader {
-    fn read(bytes: &mut Cursor<Bytes>) -> Result<Self, FromBytesError> {
-        Ok(RequestHeader {
-            api_key: FromBytes::read(bytes)?,
-            api_version: FromBytes::read(bytes)?,
-            correlation_id: FromBytes::read(bytes)?,
-            client_id: FromBytes::read(bytes)?,
-        })
-    }
-}
-
-impl ToBytes for RequestHeader {
-    fn len_to_write(&self) -> usize {
-        self.api_key.len_to_write()
-            + self.api_version.len_to_write()
-            + self.correlation_id.len_to_write()
-            + self.client_id.len_to_write()
-    }
-
-    fn write(&self, bytes: &mut BufMut) {
-        self.api_key.write(bytes);
-        self.api_version.write(bytes);
-        self.correlation_id.write(bytes);
-        self.client_id.write(bytes);
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, FromBytes, ToBytes)]
 pub struct ResponseHeader {
     pub correlation_id: i32,
-}
-
-// TODO: Derive these!
-impl FromBytes for ResponseHeader {
-    fn read(bytes: &mut Cursor<Bytes>) -> Result<Self, FromBytesError> {
-        Ok(ResponseHeader {
-            correlation_id: FromBytes::read(bytes)?,
-        })
-    }
-}
-
-impl ToBytes for ResponseHeader {
-    fn len_to_write(&self) -> usize {
-        self.correlation_id.len_to_write()
-    }
-
-    fn write(&self, bytes: &mut BufMut) {
-        self.correlation_id.write(bytes);
-    }
 }
