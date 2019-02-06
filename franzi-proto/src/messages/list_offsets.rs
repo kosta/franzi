14b237c267c8f8d4de529b90783f6d07857ec394
Kosta Welke <kw@trinatives.com>
2019-02-06 17:56:21 +0100

    list_offsets messages (all versions)

--- franzi-proto/src/messages/list_offsets.rs
+++ franzi-proto/src/messages/list_offsets.rs
@@ -1,5 +1,88 @@
-kafka_message!(
-    "ListOffsets Request (Version: 4) => replica_id isolation_level [topics]
+kafka_message!("ListOffsets Request (Version: 0) => replica_id [topics]
+  replica_id => INT32
+  topics => topic [partitions]
+    topic => STRING
+    partitions => partition timestamp max_num_offsets
+      partition => INT32
+      timestamp => INT64
+      max_num_offsets => INT32
+
+Field 	Description
+replica_id	Broker id of the follower. For normal consumers, use -1.
+topics	Topics to list offsets.
+topic	Name of topic
+partitions	Partitions to list offsets.
+partition	Topic partition id
+timestamp	The target timestamp for the partition.
+max_num_offsets	Maximum offsets to return.");
+
+kafka_message!("ListOffsets Request (Version: 1) => replica_id [topics]
+  replica_id => INT32
+  topics => topic [partitions]
+    topic => STRING
+    partitions => partition timestamp
+      partition => INT32
+      timestamp => INT64
+
+Field 	Description
+replica_id	Broker id of the follower. For normal consumers, use -1.
+topics	Topics to list offsets.
+topic	Name of topic
+partitions	Partitions to list offsets.
+partition	Topic partition id
+timestamp	The target timestamp for the partition.");
+
+kafka_message!("ListOffsets Request (Version: 2) => replica_id isolation_level [topics]
+  replica_id => INT32
+  isolation_level => INT8
+  topics => topic [partitions]
+    topic => STRING
+    partitions => partition timestamp
+      partition => INT32
+      timestamp => INT64
+
+Field 	Description
+replica_id	Broker id of the follower. For normal consumers, use -1.
+isolation_level	This setting controls the visibility of transactional
+records. Using READ_UNCOMMITTED (isolation_level = 0) makes all records
+visible. With READ_COMMITTED (isolation_level = 1), non-transactional
+and COMMITTED transactional records are visible. To be more concrete,
+READ_COMMITTED returns all data from offsets smaller than the current
+LSO (last stable offset), and enables the inclusion of the list of
+aborted transactions in the result, which allows consumers to discard
+ABORTED transactional records
+topics	Topics to list offsets.
+topic	Name of topic
+partitions	Partitions to list offsets.
+partition	Topic partition id
+timestamp	The target timestamp for the partition.");
+
+kafka_message!("ListOffsets Request (Version: 3) => replica_id isolation_level [topics]
+  replica_id => INT32
+  isolation_level => INT8
+  topics => topic [partitions]
+    topic => STRING
+    partitions => partition timestamp
+      partition => INT32
+      timestamp => INT64
+
+Field 	Description
+replica_id	Broker id of the follower. For normal consumers, use -1.
+isolation_level	This setting controls the visibility of transactional
+records. Using READ_UNCOMMITTED (isolation_level = 0) makes all records
+visible. With READ_COMMITTED (isolation_level = 1), non-transactional
+and COMMITTED transactional records are visible. To be more concrete,
+READ_COMMITTED returns all data from offsets smaller than the current
+LSO (last stable offset), and enables the inclusion of the list of
+aborted transactions in the result, which allows consumers to discard
+ABORTED transactional records
+topics	Topics to list offsets.
+topic	Name of topic
+partitions	Partitions to list offsets.
+partition	Topic partition id
+timestamp	The target timestamp for the partition.");
+
+kafka_message!("ListOffsets Request (Version: 4) => replica_id isolation_level [topics]
   replica_id => INT32
   isolation_level => INT8
   topics => topic [partitions]
@@ -11,13 +94,102 @@ kafka_message!(
 
 Field 	Description
 replica_id	Broker id of the follower. For normal consumers, use -1.
-isolation_level	This setting controls the visibility of transactional records. Using READ_UNCOMMITTED (isolation_level = 0) makes all records visible. With READ_COMMITTED (isolation_level = 1), non-transactional and COMMITTED transactional records are visible. To be more concrete, READ_COMMITTED returns all data from offsets smaller than the current LSO (last stable offset), and enables the inclusion of the list of aborted transactions in the result, which allows consumers to discard ABORTED transactional records
+isolation_level	This setting controls the visibility of transactional
+records. Using READ_UNCOMMITTED (isolation_level = 0) makes all records
+visible. With READ_COMMITTED (isolation_level = 1), non-transactional
+and COMMITTED transactional records are visible. To be more concrete,
+READ_COMMITTED returns all data from offsets smaller than the current
+LSO (last stable offset), and enables the inclusion of the list of
+aborted transactions in the result, which allows consumers to discard
+ABORTED transactional records
 topics	Topics to list offsets.
 topic	Name of topic
 partitions	Partitions to list offsets.
 partition	Topic partition id
-current_leader_epoch	The current leader epoch, if provided, is used to fence consumers/replicas with old metadata. If the epoch provided by the client is larger than the current epoch known to the broker, then the UNKNOWN_LEADER_EPOCH error code will be returned. If the provided epoch is smaller, then the FENCED_LEADER_EPOCH error code will be returned.
+current_leader_epoch	The current leader epoch, if provided, is used to
+fence consumers/replicas with old metadata. If the epoch provided by the
+client is larger than the current epoch known to the broker, then the
+UNKNOWN_LEADER_EPOCH error code will be returned. If the provided epoch
+is smaller, then the FENCED_LEADER_EPOCH error code will be returned.
 timestamp	The target timestamp for the partition.");
+
+kafka_message!("ListOffsets Response (Version: 0) => [responses]
+  responses => topic [partition_responses]
+    topic => STRING
+    partition_responses => partition error_code [offsets]
+      partition => INT32
+      error_code => INT16
+      offsets => INT64
+
+Field 	Description
+responses	The listed offsets by topic
+topic	Name of topic
+partition_responses	The listed offsets by partition
+partition	Topic partition id
+error_code	Response error code
+offsets	A list of offsets.");
+
+kafka_message!("ListOffsets Response (Version: 1) => [responses]
+  responses => topic [partition_responses]
+    topic => STRING
+    partition_responses => partition error_code timestamp offset
+      partition => INT32
+      error_code => INT16
+      timestamp => INT64
+      offset => INT64
+
+Field 	Description
+responses	The listed offsets by topic
+topic	Name of topic
+partition_responses	The listed offsets by partition
+partition	Topic partition id
+error_code	Response error code
+timestamp	The timestamp associated with the returned offset
+offset	The offset found");
+
+kafka_message!("ListOffsets Response (Version: 2) => throttle_time_ms [responses]
+  throttle_time_ms => INT32
+  responses => topic [partition_responses]
+    topic => STRING
+    partition_responses => partition error_code timestamp offset
+      partition => INT32
+      error_code => INT16
+      timestamp => INT64
+      offset => INT64
+
+Field 	Description
+throttle_time_ms	Duration in milliseconds for which the request was
+throttled due to quota violation (Zero if the request did not violate
+any quota)
+responses	The listed offsets by topic
+topic	Name of topic
+partition_responses	The listed offsets by partition
+partition	Topic partition id
+error_code	Response error code
+timestamp	The timestamp associated with the returned offset
+offset	The offset found");
+
+kafka_message!("ListOffsets Response (Version: 3) => throttle_time_ms [responses]
+  throttle_time_ms => INT32
+  responses => topic [partition_responses]
+    topic => STRING
+    partition_responses => partition error_code timestamp offset
+      partition => INT32
+      error_code => INT16
+      timestamp => INT64
+      offset => INT64
+
+Field 	Description
+throttle_time_ms	Duration in milliseconds for which the request was
+throttled due to quota violation (Zero if the request did not violate
+any quota)
+responses	The listed offsets by topic
+topic	Name of topic
+partition_responses	The listed offsets by partition
+partition	Topic partition id
+error_code	Response error code
+timestamp	The timestamp associated with the returned offset
+offset	The offset found");
 
 kafka_message!("ListOffsets Response (Version: 4) => throttle_time_ms [responses]
   throttle_time_ms => INT32
@@ -31,7 +203,9 @@ kafka_message!("ListOffsets Response (Version: 4) => throttle_time_ms [responses
       leader_epoch => INT32
 
 Field 	Description
-throttle_time_ms	Duration in milliseconds for which the request was throttled due to quota violation (Zero if the request did not violate any quota)
+throttle_time_ms	Duration in milliseconds for which the request was
+throttled due to quota violation (Zero if the request did not violate
+any quota)
 responses	The listed offsets by topic
 topic	Name of topic
 partition_responses	The listed offsets by partition
