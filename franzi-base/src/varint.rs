5420b183f3c17ef26a9980dabe041335f62bc78f
Kosta Welke <kw@trinatives.com>
2019-02-17 23:47:08 +0100

    varint: fix clippy warnings

--- franzi-base/src/varint.rs
+++ franzi-base/src/varint.rs
@@ -21,30 +21,30 @@ fn read_u8(bytes: &mut Cursor<Bytes>) -> Result<u8, FromBytesError> {
 pub fn read_varint32(bytes: &mut Cursor<Bytes>) -> Result<u32, FromBytesError> {
     let mut b = read_u8(bytes)?;
     if b & 0x80 == 0 {
-        return Ok(b as u32);
+        return Ok(u32::from(b));
     }
-    let mut r = (b & 0x7f) as u32;
+    let mut r = u32::from(b & 0x7f);
 
     b = read_u8(bytes)?;
-    r |= ((b & 0x7f) as u32) << 7;
+    r |= (u32::from(b & 0x7f)) << 7;
     if b & 0x80 == 0 {
         return Ok(r);
     }
 
     b = read_u8(bytes)?;
-    r |= ((b & 0x7f) as u32) << 14;
+    r |= (u32::from(b & 0x7f)) << 14;
     if b & 0x80 == 0 {
         return Ok(r);
     }
 
     b = read_u8(bytes)?;
-    r |= ((b & 0x7f) as u32) << 21;
+    r |= (u32::from(b & 0x7f)) << 21;
     if b & 0x80 == 0 {
         return Ok(r);
     }
 
     b = read_u8(bytes)?;
-    r |= ((b & 0xf) as u32) << 28;
+    r |= (u32::from(b & 0xf)) << 28;
     if b & 0x80 == 0 {
         return Ok(r);
     }
@@ -66,64 +66,64 @@ pub fn read_varint64(bytes: &mut Cursor<Bytes>) -> Result<u64, FromBytesError> {
     // part0
     let mut b = read_u8(bytes)?;
     if b & 0x80 == 0 {
-        return Ok(b as u64);
+        return Ok(u64::from(b));
     }
-    let mut r0 = (b & 0x7f) as u32;
+    let mut r0 = u32::from(b & 0x7f);
 
     b = read_u8(bytes)?;
-    r0 |= ((b & 0x7f) as u32) << 7;
+    r0 |= (u32::from(b & 0x7f)) << 7;
     if b & 0x80 == 0 {
-        return Ok(r0 as u64);
-    }
-
-    b = read_u8(bytes)?;
-    r0 |= ((b & 0x7f) as u32) << 14;
-    if b & 0x80 == 0 {
-        return Ok(r0 as u64);
+        return Ok(u64::from(r0));
     }
 
     b = read_u8(bytes)?;
-    r0 |= ((b & 0x7f) as u32) << 21;
+    r0 |= (u32::from(b & 0x7f)) << 14;
     if b & 0x80 == 0 {
-        return Ok(r0 as u64);
+        return Ok(u64::from(r0));
+    }
+
+    b = read_u8(bytes)?;
+    r0 |= (u32::from(b & 0x7f)) << 21;
+    if b & 0x80 == 0 {
+        return Ok(u64::from(r0));
     }
 
     // part1
     b = read_u8(bytes)?;
-    let mut r1 = (b & 0x7f) as u32;
+    let mut r1 = u32::from(b & 0x7f);
     if b & 0x80 == 0 {
-        return Ok(r0 as u64 | (r1 as u64) << 28);
+        return Ok(u64::from(r0) | u64::from(r1) << 28);
     }
 
     b = read_u8(bytes)?;
-    r1 |= ((b & 0x7f) as u32) << 7;
+    r1 |= (u32::from(b & 0x7f)) << 7;
     if b & 0x80 == 0 {
-        return Ok(r0 as u64 | (r1 as u64) << 28);
+        return Ok(u64::from(r0) | u64::from(r1) << 28);
     }
 
     b = read_u8(bytes)?;
-    r1 |= ((b & 0x7f) as u32) << 14;
+    r1 |= (u32::from(b & 0x7f)) << 14;
     if b & 0x80 == 0 {
-        return Ok(r0 as u64 | (r1 as u64) << 28);
+        return Ok(u64::from(r0) | u64::from(r1) << 28);
     }
 
     b = read_u8(bytes)?;
-    r1 |= ((b & 0x7f) as u32) << 21;
+    r1 |= (u32::from(b & 0x7f)) << 21;
     if b & 0x80 == 0 {
-        return Ok(r0 as u64 | (r1 as u64) << 28);
+        return Ok(u64::from(r0) | u64::from(r1) << 28);
     }
 
     // part2
     b = read_u8(bytes)?;
-    let mut r2 = (b & 0x7f) as u32;
+    let mut r2 = u32::from(b & 0x7f);
     if b & 0x80 == 0 {
-        return Ok((r0 as u64 | (r1 as u64) << 28) | (r2 as u64) << 56);
+        return Ok((u64::from(r0) | u64::from(r1) << 28) | u64::from(r2) << 56);
     }
 
     b = read_u8(bytes)?;
-    r2 |= (b as u32) << 7;
+    r2 |= (u32::from(b)) << 7;
     if b & 0x80 == 0 {
-        return Ok((r0 as u64 | (r1 as u64) << 28) | (r2 as u64) << 56);
+        return Ok((u64::from(r0) | u64::from(r1) << 28) | u64::from(r2) << 56);
     }
 
     // cannot read more than 10 bytes
@@ -135,13 +135,13 @@ pub fn sizeof_varint(v: u64) -> usize {
     match v {
         0x0...0x7F => 1,
         0x80...0x3FFF => 2,
-        0x4000...0x1FFFFF => 3,
-        0x200000...0xFFFFFFF => 4,
-        0x10000000...0x7FFFFFFFF => 5,
-        0x0800000000...0x3FFFFFFFFFF => 6,
-        0x040000000000...0x1FFFFFFFFFFFF => 7,
-        0x02000000000000...0xFFFFFFFFFFFFFF => 8,
-        0x0100000000000000...0x7FFFFFFFFFFFFFFF => 9,
+        0x4000...0x1F_FFFF => 3,
+        0x20_0000...0xFFF_FFFF => 4,
+        0x1000_0000...0x007_FFFF_FFFF => 5,
+        0x08_0000_0000...0x03FF_FFFF_FFFF => 6,
+        0x0400_0000_0000...0x0001_FFFF_FFFF_FFFF => 7,
+        0x0002_0000_0000_0000...0x00FF_FFFF_FFFF_FFFF => 8,
+        0x0100_0000_0000_0000...0x7FFF_FFFF_FFFF_FFFF => 9,
         _ => 10,
     }
 }
